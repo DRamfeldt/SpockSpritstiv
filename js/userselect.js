6c85e4583ad803705ca72f576b152c826d27af10
SpockSpritstiv <DRamfeldt@Gmail.com>
2019-08-02 14:46:38 +0200

    Update userselect.js

--- js/userselect.js
+++ js/userselect.js
@@ -26,7 +26,6 @@ var app = new function() {
     ["Fuldemir", ["eA9u-MF_H6Y","VrAu6ve8F6Y","oxWfTs1psoM"], "profil/simon.jpg", true],
     ["Lars", ["0HENeKIztcQ"], "profil/Oscar.jpg", true],
     ["Såla’", ["unfzfe8f9NI","KoyNlVQbUPc","XEjLoHdbVeE"], "profil/Marie.jpg", true],
-    ["Alle", ["VEVDcuqW01Y", "AgFeZr5ptV8", "0NTDqqnH9Bk","04854XqcfCY"], "profil/alle.jpg", true],
     ["Darth", ["4fndeDfaWCg", "POq2AznJO1Q", "aBt8fN7mJNg", "5rmKy8H62BU"], "profil/Rolf.jpg", true]
     ];
 
