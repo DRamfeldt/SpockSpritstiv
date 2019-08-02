dea679c626f846d45c4df33543bd59d3ee3bc1e2
SpockSpritstiv <DRamfeldt@Gmail.com>
2019-08-02 14:31:58 +0200

    alle

--- js/game.js
+++ js/game.js
@@ -36,6 +36,7 @@ var items = [
     ["Fuldemir", ["eA9u-MF_H6Y","VrAu6ve8F6Y","oxWfTs1psoM"], "profil/simon.jpg"],
     ["Lars", ["0HENeKIztcQ"], "profil/Oscar.jpg"],
     ["Såla’", ["unfzfe8f9NI","KoyNlVQbUPc","XEjLoHdbVeE"], "profil/Marie.jpg"],
+    ["Alle", ["VEVDcuqW01Y", "AgFeZr5ptV8", "0NTDqqnH9Bk","04854XqcfCY"], "profil/alle.jpg"],
     ["Darth", ["4fndeDfaWCg", "POq2AznJO1Q", "aBt8fN7mJNg", "5rmKy8H62BU"], "profil/Rolf.jpg"]
   ];
 
